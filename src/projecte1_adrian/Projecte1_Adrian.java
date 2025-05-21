ca5ae06d14761854f0b9d718f8a3645c1daaad25
adrian.arboledasa@educem.net <arboledas.sandbox@gmail.com>
2025-05-21 20:03:48 +0200

    Primer commit

---
+++ src/projecte1_adrian/Projecte1_Adrian.java
@@ -0,0 +1,14 @@
+package projecte1_adrian;
+
+
+public class Projecte1_Adrian {
+
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola");
+    }
+    
+    
+}
