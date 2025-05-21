56a94c2366659c52af9b36932b2e4dd7a24ceb67
adrian.arboledasa@educem.net <arboledas.sandbox@gmail.com>
2025-05-21 20:16:25 +0200

    Segundo commit

--- src/projecte1_adrian/Projecte1_Adrian.java
+++ src/projecte1_adrian/Projecte1_Adrian.java
@@ -1,14 +1,11 @@
 package projecte1_adrian;
 
-
 public class Projecte1_Adrian {
 
-
     public static void main(String[] args) {
-        
-        
-        System.out.println("Hola");
+
+        System.out.println("HOLA");
+        System.out.println("Adeu");
     }
-    
-    
+
 }
