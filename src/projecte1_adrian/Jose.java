fa2eeab740d42494d19107773047e35753ab146d
Jose Caoç <joselitus05@exemple.com>
2025-05-22 19:14:15 +0200

    Nueva rama

--- src/projecte1_adrian/Jose.java
+++ src/projecte1_adrian/Jose.java
@@ -2,4 +2,19 @@ package projecte1_adrian;
 
 public class Jose {
     
+    private String nom;
+
+    public Jose(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
 }
